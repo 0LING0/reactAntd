91f0191c5a7a5dbd1a7bb0ee76a950b8c921da72
ling <2876248147@qq.com>
2019-01-07 18:56:40 +0800

    no message

--- imoocmanager/src/pages/table/basicTable.js
+++ imoocmanager/src/pages/table/basicTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Card,Table} from 'antd';
-// import 
+import {Card,Table, Modal, message, Button} from 'antd';
+import axios from './../../axios/index';
 export default class BasicTable extends React.Component{
     state = {};
     componentDidMount(){
@@ -36,13 +36,66 @@ export default class BasicTable extends React.Component{
                 time: '09:00'
             }
         ]
+        dataSource.map((item,index)=>{
+            item.key = index;
+        });
         this.setState({
             dataSource
         });
+        this.request();
     }
 
     // 动态获取mock数据
-    request = ()=>{}
+    request = ()=>{
+        axios.ajax({
+            url: '/table/list',
+            data:{
+                params:{
+                    page: 1
+                },
+                isShowLoading: true
+            }
+        }).then((res)=>{
+            if(res.code === 0){
+                res.result.map((item,index)=>{
+                    item.key = index;
+                });
+                this.setState({
+                    dataSource2: res.result,
+                    selectedRowKeys:[],
+                    selectedRows: null
+                });
+            }
+        });
+    }
+
+    onRowClick = (record,index)=>{
+        let selectKey = [index];
+        Modal.info({
+            title:'信息',
+            content: `用户名: ${record.userName},用户爱好: ${record.interest}`
+        });
+        this.setState({
+            selectedRowKeys: selectKey,
+            selectedItem: record
+        });
+    }
+
+    // 多选执行删除动作
+    handleDelete = (()=>{
+        let rows = this.state.selectedRows;
+        let ids = [];
+        rows.map((item)=>{
+            ids.push(item.id);
+        });
+        Modal.confirm({
+            title:'删除提示',
+            content:`您确定要删除这些数据吗? ${ids.join(',')}`,
+            onOk: ()=>{
+                message.success('删除成功');
+            }
+        });
+    });
 
     render(){
         const columns = [
@@ -56,15 +109,41 @@ export default class BasicTable extends React.Component{
             },
             {
                 title: '性别',
-                dataIndex: 'sex'
+                dataIndex: 'sex',
+                render(sex){
+                    return sex == 1 ? '男':'女'
+                }
             },
             {
                 title: '状态',
-                dataIndex: 'state'
+                dataIndex: 'state',
+                render(state){
+                    let config = {
+                        '1': '咸鱼一条',
+                        '2': '风华浪子',
+                        '3': '北大才子',
+                        '4': '百度FE',
+                        '5': '创业者'
+                    }
+                    return config[state];
+                }
             },
             {
                 title: '爱好',
-                dataIndex: 'interest'
+                dataIndex: 'interest',
+                render(abc){
+                    let config = {
+                        '1': '游泳',
+                        '2': '打篮球',
+                        '3': '踢足球',
+                        '4': '爬山',
+                        '5': '骑行',
+                        '6': '跑步',
+                        '7': '桌球',
+                        '8': '麦霸'
+                    }
+                    return config[abc];
+                }
             },
             {
                 title: '生日',
@@ -79,6 +158,25 @@ export default class BasicTable extends React.Component{
                 dataIndex: 'time'
             }
         ]
+        const {selectedRowKeys} = this.state;
+        const rowSelection = {
+            type: 'radio',
+            selectedRowKeys
+        }
+        const rowCheckSelection = {
+            type: 'checkbox',
+            selectedRowKeys,
+            onChange: (selectedRowKeys,selectedRows)=>{
+                let ids = [];
+                selectedRows.map((item)=>{
+                    ids.push(item.id);
+                });
+                this.setState({
+                    selectedRowKeys,
+                    selectedRows
+                })
+            }
+        }
         return(
             <div>
                 <Card title="基础表格">
@@ -93,7 +191,42 @@ export default class BasicTable extends React.Component{
                     <Table
                         bordered
                         columns = {columns}
-                        dataSource = {this.state.dataSource}
+                        dataSource = {this.state.dataSource2}
+                        pagination={false}
+                    />
+                </Card>
+                <Card title="Mock-单选" style={{margin:'10px 0'}}>
+                    <Table
+                        bordered
+                        rowSelection = {rowSelection}
+                        onRow={(record,index) => {
+                            return {
+                                onClick: () =>{
+                                    this.onRowClick(record,index);
+                                },
+                            }
+                        }}
+                        columns = {columns}
+                        dataSource = {this.state.dataSource2}
+                        pagination={false}
+                    />
+                </Card>
+                <Card title="Mock-复选" style={{margin:'10px 0'}}>
+                    <div style={{marginBottom:10}}>
+                        <Button onClick={this.handleDelete}>删除</Button>
+                    </div>
+                    <Table
+                        bordered
+                        rowSelection = {rowCheckSelection}
+                        onRow={(record,index) => {
+                            return {
+                                onClick: () =>{
+                                    this.onRowClick(record,index);
+                                },
+                            }
+                        }}
+                        columns = {columns}
+                        dataSource = {this.state.dataSource2}
                         pagination={false}
                     />
                 </Card>
